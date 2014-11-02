6d9970340dc91842a9769890872c21020c5481b2
Oz <ozsoreq@gmail.com>
2014-11-02 19:30:48 +0200

    Added Hello to string

--- src/com/mta/javacourse/Project1Servlet.java
+++ src/com/mta/javacourse/Project1Servlet.java
@@ -6,6 +6,6 @@ import javax.servlet.http.*;
 public class Project1Servlet extends HttpServlet {
 	public void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
 		resp.setContentType("text/plain");
-		resp.getWriter().println("Oz Soreq");
+		resp.getWriter().println("Oz Soreq, Hello");
 	}
 }
